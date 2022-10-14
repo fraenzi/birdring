0c75258c4ade1f5a7765a7de8c09a344490c9d6c
Fränzi Korner <fraenzi.korner@vogelwarte.ch>
2022-10-14 10:11:53 +0200

    updated the function decimal.coord so that it can handle seconds

--- R/decimal.coord.r
+++ R/decimal.coord.r
@@ -2,12 +2,17 @@
 # Computes decimal coordinates from degrees and minutes
 # x: vector containing degrees and minutes
 # Author: Fraenzi Korner, 9.9.2004, www.oikostat.ch and www.vogelwarte.ch
+#         updated 14.10.2022 to include seconds
 ###################################################################################
 decimal.coord<-function(x){
-t.sign<-sign(x)
-t.minuten<-abs(x)-abs(trunc(x))
-t.dezimalstellen<-t.minuten*(5/3)
-t.dec<-t.sign*(abs(trunc(x))+t.dezimalstellen)
-t.dec
+  # x is a numeric vector of the following format dd.mmss, e.g. 55.3215 for 55degree 32' 15''
+  t.sign<-sign(x)
+  t.min <-floor(100*(abs(x)-abs(trunc(x))))/100
+  t.sec <- abs(100*x)-abs(trunc(100*x))
+  t.dec1 <- t.min*(5/3)
+  t.dec2 <- t.sec*(5/3)
+  t.decimals <- t.dec1 + t.dec2/100
+  t.dec<-t.sign*(abs(trunc(x))+t.decimals)
+  t.dec
 }
 ###################################################################################
