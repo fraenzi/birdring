2a3120f2644312cb3f2b90422c5356aa4cbb9797
Fränzi Korner <fraenzi.korner@vogelwarte.ch>
2023-05-10 12:40:11 +0200

    decimal coordinates corrected

--- R/decimal.coord.r
+++ R/decimal.coord.r
@@ -5,13 +5,12 @@
 #         updated 14.10.2022 to include seconds
 ###################################################################################
 decimal.coord<-function(x){
-  # x is a numeric vector of the following format dd.mmss, e.g. 55.3215 for 55degree 32' 15''
+  # x is a numeric vector of the following format dd.mmss, e.g. 55.3215 for 
+  # 55degree 32' 15''
   t.sign<-sign(x)
-  t.min <-floor(100*(abs(x)-abs(trunc(x))))/100
-  t.sec <- abs(100*x)-abs(trunc(100*x))
-  t.dec1 <- t.min*(5/3)
-  t.dec2 <- t.sec*(5/3)
-  t.decimals <- t.dec1 + t.dec2/100
+  t.min <-floor(100*(abs(x)-abs(trunc(x))))
+  t.sec <- 100*(abs(100*x)-abs(trunc(100*x)))
+  t.decimals <- t.min/60 + t.sec/3600
   t.dec<-t.sign*(abs(trunc(x))+t.decimals)
   t.dec
 }
